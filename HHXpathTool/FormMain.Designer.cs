d6c0e0aa7238fc545921649fe9be51508dab8b06
niuniu <42614527+dahaoniuniu@users.noreply.github.com>
2018-10-11 16:24:00 +0800

    ok

---
+++ HHXpathTool/FormMain.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace HHXpath
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richtbXpath = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.richtbRsult = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Xpath = new System.Windows.Forms.TextBox();
+            this.txt_Attr = new System.Windows.Forms.TextBox();
+            this.btn_Check = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.richtbXpath);
+            this.groupBox1.Location = new System.Drawing.Point(5, 4);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(471, 868);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Html";
+            // 
+            // richtbXpath
+            // 
+            this.richtbXpath.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richtbXpath.Location = new System.Drawing.Point(7, 24);
+            this.richtbXpath.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.richtbXpath.Name = "richtbXpath";
+            this.richtbXpath.Size = new System.Drawing.Size(458, 500);
+            this.richtbXpath.TabIndex = 0;
+            this.richtbXpath.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.richtbRsult);
+            this.groupBox2.Location = new System.Drawing.Point(3, 4);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Size = new System.Drawing.Size(568, 860);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Result";
+            // 
+            // richtbRsult
+            // 
+            this.richtbRsult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richtbRsult.Location = new System.Drawing.Point(15, 24);
+            this.richtbRsult.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.richtbRsult.Name = "richtbRsult";
+            this.richtbRsult.Size = new System.Drawing.Size(545, 500);
+            this.richtbRsult.TabIndex = 1;
+            this.richtbRsult.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Xpath规则";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(668, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "属性";
+            // 
+            // txt_Xpath
+            // 
+            this.txt_Xpath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_Xpath.Location = new System.Drawing.Point(87, 14);
+            this.txt_Xpath.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_Xpath.Name = "txt_Xpath";
+            this.txt_Xpath.Size = new System.Drawing.Size(557, 23);
+            this.txt_Xpath.TabIndex = 4;
+            this.txt_Xpath.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_Xpath_KeyDown);
+            // 
+            // txt_Attr
+            // 
+            this.txt_Attr.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_Attr.Location = new System.Drawing.Point(706, 14);
+            this.txt_Attr.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txt_Attr.Name = "txt_Attr";
+            this.txt_Attr.Size = new System.Drawing.Size(273, 23);
+            this.txt_Attr.TabIndex = 5;
+            // 
+            // btn_Check
+            // 
+            this.btn_Check.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Check.Location = new System.Drawing.Point(985, 12);
+            this.btn_Check.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_Check.Name = "btn_Check";
+            this.btn_Check.Size = new System.Drawing.Size(87, 26);
+            this.btn_Check.TabIndex = 6;
+            this.btn_Check.Text = "匹配测试";
+            this.btn_Check.UseVisualStyleBackColor = true;
+            this.btn_Check.Click += new System.EventHandler(this.btn_Check_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(15, 45);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(1057, 541);
+            this.splitContainer1.SplitterDistance = 479;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 598);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.btn_Check);
+            this.Controls.Add(this.txt_Attr);
+            this.Controls.Add(this.txt_Xpath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "FormMain";
+            this.Text = "Xpath调试工具";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_Xpath;
+        private System.Windows.Forms.TextBox txt_Attr;
+        private System.Windows.Forms.Button btn_Check;
+        private System.Windows.Forms.RichTextBox richtbXpath;
+        private System.Windows.Forms.RichTextBox richtbRsult;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ HHXpathTool/FormMain.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace HHXpath
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Check_Click(object sender, EventArgs e)
+        {
+            CheckXpath();
+        }
+
+        private void txt_Xpath_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                CheckXpath();
+            }
+
+        }
+        public void CheckXpath()
+        {
+            HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
+            htmlDocument.LoadHtml(this.richtbXpath.Text);
+            try
+            {
+                HtmlNodeCollection htmlNodeCollection = htmlDocument.DocumentNode.SelectNodes(this.txt_Xpath.Text.Trim());
+                this.richtbRsult.Text = "";
+                bool flag = htmlNodeCollection == null;
+                if (flag)
+                {
+                    this.richtbRsult.Text = @"=====未匹配到节点，请检查=====";
+                }
+                else
+                {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    int num = 0;
+                    foreach (HtmlNode current in ((IEnumerable<HtmlNode>)htmlNodeCollection))
+                    {
+                        num++;
+                        bool flag2 = !string.IsNullOrEmpty(this.txt_Attr.Text);
+                        if (flag2)
+                        {
+                            try
+                            {
+                                stringBuilder.Append(string.Concat(new object[]
+                                {
+                                    "[",
+                                    num,
+                                    "]     ",
+                                    current.Attributes[this.txt_Attr.Text].Value
+                                }));
+                                string attributeValue = current.GetAttributeValue("href", "");
+                            }
+                            catch (Exception)
+                            {
+                                stringBuilder.Append("[" + num + "]=====未查询到此属性，请检查=====");
+                            }
+                        }
+                        else
+                        {
+                            stringBuilder.Append(string.Concat(new object[]
+                            {
+                                "[",
+                                num,
+                                "]     ",
+                                current.InnerText
+                            }));
+                        }
+                        stringBuilder.Append("\r\n--------------------------------------------------\r\n");
+                    }
+                    this.richtbRsult.Text = stringBuilder.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                this.richtbRsult.Text = ex.Message;
+            }
+        }
+    }
+}
